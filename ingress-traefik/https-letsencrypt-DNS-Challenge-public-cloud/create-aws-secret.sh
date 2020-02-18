e8dbcadb79c265ec6555f14f5891fdb13de5be21
Kamran Azeem <kaz@praqma.net>
2020-02-18 23:57:54 +0100

    fixed namespace for AWS secret

--- ingress-traefik/https-letsencrypt-DNS-Challenge-public-cloud/create-aws-secret.sh
+++ ingress-traefik/https-letsencrypt-DNS-Challenge-public-cloud/create-aws-secret.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-kubectl create secret generic secret-aws-dns-credentials \
-  --from-literal=AWS_ACCESS_KEY_ID=AKIAV6U4CCTT6352PUPX \
-  --from-literal=AWS_SECRET_ACCESS_KEY=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 \
+kubectl --namespace=kube-system create secret generic secret-aws-dns-credentials \
+  --from-literal=AWS_ACCESS_KEY_ID=AKIAV6U4CCTT5IZOOUGV \
+  --from-literal=AWS_SECRET_ACCESS_KEY=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 \
   --from-literal=AWS_DEFAULT_REGION=eu-central-1 \
   --from-literal=AWS_HOSTED_ZONE_ID=ZO5ESCGCDQT6O
 
