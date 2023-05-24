4ee7a8c81495c1d2ab41a514ee75752626e5bc45
zanderhavgaard <zander@havgaard.dk>
2023-05-24 14:32:21 +0200

    fix comment in deployments-loadbalancing referencing frontend where it should have been backend, apply some formatting to deployments-loadbalancing files

--- deployments-loadbalancing/start/poll.sh
+++ deployments-loadbalancing/start/poll.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 #check if any parameters are passed
-if [ $# -eq 0 ]
-then
-    echo "No arguments supplied, please pass the ip address of the server"
-    echo "And remember the port as well"
-    exit 1
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied, please pass the ip address of the server"
+	echo "And remember the port as well"
+	exit 1
 fi
-while true; do  curl --connect-timeout 1 -m 1 -s $1; sleep 0.5; done
+while true; do
+	curl --connect-timeout 1 -m 1 -s $1
+	sleep 0.5
+done
